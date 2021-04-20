ce77d9c7db0a87e54b10ed7347c342a446972119
Osman Beltran <osmancadc@hotmail.com>
2021-04-20 09:02:09 -0500

    Subida de archivos - Bug corregido

--- fileManager/controller/pdfController.js
+++ fileManager/controller/pdfController.js
@@ -1,47 +1,24 @@
-const router = require('express').Router()
-const bodyParser = require('body-parser')
-const fs = require('fs')
-const pdf2table = require('pdf2table');
-const md5 = require('md5');
-const dbController = require('./databaseController')
+exports.handler = async (event) => {
+    const pdf2table = require('pdf2table');
+    const {
+        Base64
+    } = require('js-base64');
 
-const {
-    Base64
-} = require('js-base64');
-
-
-router.use(bodyParser.json({
-    limit: '50mb'
-}));
-
-router.post("/upload", async (req, res) => {
     console.log("Recibio solicitud de subida PDF")
 
-    var bin = Base64.atob(req.body.file);
-    var datetime = new Date();
+    var bin = Base64.atob(event.file);
+    let buffer = Buffer.from(bin, 'binary')
 
-    file_name = `extract_${md5(datetime.toUTCString())}}.pdf`
+    const promise = new Promise(function (resolve, reject) {
+        pdf2table.parse(buffer, function (err, rows, rowsdebug) {
+            console.log("Termino de parsear")
+            if (err) return console.error(err)
+            //rows = rows.filter(e => e[0].indexOf('/') != -1)
+            resolve({
+                status: 200
+            })
+        });
+    })
+    return promise
 
-    fs.writeFile(file_name, bin, 'binary', error => {
-        if (error)
-            throw error;
-        else {
-            console.log('Archivo recibido correctamente!');
-            fs.readFile(file_name, function (err, buffer) {
-                if (err) return console.error(err);
-
-                pdf2table.parse(buffer, function (err, rows, rowsdebug) {
-                    if (err) return console.error(err)
-                    rows = rows.filter(e => e[0].indexOf('/') != -1)
-                    dbController.insertMovements(rows, 'osmancadc@hotmail.com', 154597)
-                    res.status(200).send("Información subida con exito")
-                });
-            });
-        }
-    });
-
-});
-
-
-
-module.exports = router
+};
