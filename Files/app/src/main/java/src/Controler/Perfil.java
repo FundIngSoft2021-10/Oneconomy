02b303f49f02097f22481fe43c187eed5c917c1a
Santiago Rodríguez <srodri0715@gmail.com>
2021-04-20 21:51:37 -0500

    Cambios a ingreso manual y pantalla perfil

--- Files/app/src/main/java/src/Controler/Perfil.java
+++ Files/app/src/main/java/src/Controler/Perfil.java
@@ -3,6 +3,8 @@ package src.Controler;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 import com.example.oneconomy.R;
 
@@ -12,5 +14,13 @@ public class Perfil extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_perfil);
+
+        TextView nombre = (TextView) findViewById(R.id.textoNombre);
+        TextView nombreUsuario = (TextView) findViewById(R.id.textoNombreUsuario);
+
+
+        nombre.setText("prueba");
+        nombreUsuario.setText("prueba");
+
     }
 }
