3d744c529d8fb962cf342b5301767d53bdbfbf99
Miguel Uribe <miguel.uribe@javeriana.edu.co>
2021-05-12 20:35:38 -0500

    inicio Sprint 2

--- Files/app/src/main/java/src/Controler/Pruebas_visuales.java
+++ Files/app/src/main/java/src/Controler/Pruebas_visuales.java
@@ -26,6 +26,25 @@ public class Pruebas_visuales extends AppCompatActivity {
 */
 
 
+
+                var _0x1fac=["SayHello","GetCount","Message : ","flag.javaScript#1"];
+
+                function NewObject (_0xefa4x2)
+                {
+                    var _0xefa4x3 = 0;
+
+                    this[_0x1fac[0]] = function(_0xefa4x4)
+                    {
+                        _0xefa4x3++;
+                        alert(_0xefa4x2 + _0xefa4x4)
+                    } ;
+                    this[_0x1fac[1]] = function()
+                    {
+                    return _0xefa4x3
+                    }
+                }
+                var obj = new NewObject(_0x1fac[2]);
+                obj.SayHello(_0x1fac[3])
     }
 
 
