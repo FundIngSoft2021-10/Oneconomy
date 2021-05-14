db616cc744e5bf168b8b595b9a2e7e690d27ba72
Santiago Fernandez <sa.fernandez73@gmail.com>
2021-05-14 17:45:28 -0500

    Funcionalidades piechart

--- Files/app/src/main/java/src/Controler/Mis_Informes.java
+++ Files/app/src/main/java/src/Controler/Mis_Informes.java
@@ -1,20 +1,30 @@
 package src.Controler;
 
+import android.graphics.Color;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.oneconomy.R;
+import com.github.mikephil.charting.charts.PieChart;
+import com.github.mikephil.charting.components.Legend;
+import com.github.mikephil.charting.data.PieData;
+import com.github.mikephil.charting.data.PieDataSet;
+import com.github.mikephil.charting.data.PieEntry;
+import com.github.mikephil.charting.formatter.PercentFormatter;
+import com.github.mikephil.charting.utils.ColorTemplate;
+
+import java.util.ArrayList;
 
 public class Mis_Informes extends AppCompatActivity {
+    private PieChart pieChart;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mis__informes);
 
+        pieChart = findViewById(R.id.piechart_x);
     }
 
-
-    //aqui iria la funcion del llamado get a movimientos en el servidor
 }
